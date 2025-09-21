a26d51705bde7998373be6c30302493e4c297a15
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2025-09-20 22:59:38 -0700

    Add PlayButtons component to MainMenu

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,9 +1,13 @@
+import PlayButtons from "./PlayButtons"
+
 export default function MainMenu() {
+
     return (
         <section id="main-menu">
             <div className="container">
                 <h1>Pokemon Memory Game</h1>
                 <p>Memorize the pokemon on your screen. Try to catch them all!</p>
+                <PlayButtons />
             </div>
         </section>
     )
