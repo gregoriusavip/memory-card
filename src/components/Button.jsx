23919f67c2a1711fa27f574d9ffb41d9c122c9b2
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2025-09-23 21:51:39 -0700

    Refactor to detach button with GameButton

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,28 +1,8 @@
 import '../styles/button.css'
-import { useQueryClient } from '@tanstack/react-query'
 
-async function fetchGeneration(id) {
-    const response = await fetch(`https://pokeapi.co/api/v2/generation/${id}`)
-    return await response.json()
-}
-
-export default function Button({ text, options, setOptions, id }) {
-    const queryClient = useQueryClient();
-
-    async function handleClick() {
-        const data = await queryClient.fetchQuery({
-            queryKey: ['generation', id],
-            queryFn: async () => await fetchGeneration(id)
-        })
-        setOptions({
-            ...options,
-            generationData: data,
-            generationId: id
-        });
-    }
-
+export default function Button({ text, handleClick }) {
     return (
-        <button onClick={handleClick} disabled={(options.generationData ? true : false)}>
+        <button onClick={handleClick}>
             {text}
         </button>
     )
--- src/components/GameButtons.jsx
+++ src/components/GameButtons.jsx
@@ -1,11 +1,31 @@
 import Button from "./Button"
+import { useQueryClient } from "@tanstack/react-query"
 import '../styles/playButton.css'
 
+async function fetchGeneration(id) {
+    const response = await fetch(`https://pokeapi.co/api/v2/generation/${id}`)
+    return await response.json()
+}
+
 export default function GameButtons({ options, setOptions }) {
+    const queryClient = useQueryClient();
+
+    async function handleClick(id) {
+        const data = await queryClient.fetchQuery({
+            queryKey: ['generation', id],
+            queryFn: async () => await fetchGeneration(id)
+        })
+        setOptions({
+            ...options,
+            generationData: data,
+            generationId: id
+        });
+    }
+
     return (
         <div className="button-container">
-            <Button text='GENERATION 1' options={options} setOptions={setOptions} id={1}></Button>
-            <Button text='GENERATION 2' options={options} setOptions={setOptions} id={2}></Button>
+            <Button text='GENERATION 1' handleClick={() => handleClick(1)}></Button>
+            <Button text='GENERATION 2' handleClick={() => handleClick(2)}></Button>
         </div>
     )
 }
