abcdf736793650272b2457b2254dc9851a393c29
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2025-09-25 11:11:55 -0700

    Move fetch to GameRound

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,40 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
 import CardButtons from "./CardButtons";
-import Loader from "./Loader";
 import "../styles/card.css";
 import { useState } from "react";
 
-async function fetchPokemon(name) {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-  return await response.json();
-}
-
-export default function PokeCard({ pokemonName, setPlayerStats, playerStats }) {
+export default function PokeCard({ pokemonName, imgUrl }) {
   const [clickedButton, setClickedButton] = useState(null);
-  let url = "";
-  const { isPending, isError, data, isSuccess, error } = useQuery({
-    queryKey: ["pokemon", pokemonName],
-    queryFn: async () => await fetchPokemon(pokemonName),
-  });
-
-  if (isPending) {
-    return <Loader></Loader>;
-  }
-
-  if (isError) {
-    // TODO: Error Pop up
-    return <span>Error: {error.message}</span>;
-  }
-
-  if (isSuccess) {
-    url = data["sprites"]["other"]["official-artwork"]["front_default"];
-  }
 
   return (
     <div className={"card" + (clickedButton ? " clicked" : "")}>
       <img
         className="sprite"
-        src={url}
+        src={imgUrl}
         alt={"the sprite of the pokemon " + pokemonName}
       ></img>
       <h2>{pokemonName.toUpperCase()}</h2>
--- src/components/GameRound.jsx
+++ src/components/GameRound.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import PokeCard from "./Card";
+import Loader from "./Loader";
+import { useQuery } from "@tanstack/react-query";
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
+}
+
+async function fetchPokemon(name) {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  return await response.json();
 }
 
 export default function GameRound({ pokemonList }) {
@@ -14,11 +21,22 @@ export default function GameRound({ pokemonList }) {
     score: 0,
   });
 
-  return (
-    <PokeCard
-      pokemonName={pokemonName}
-      setPlayerStats={setPlayerStats}
-      playerStats={playerStats}
-    ></PokeCard>
-  );
+  const { isPending, isError, data, isSuccess, error } = useQuery({
+    queryKey: ["pokemon", pokemonName],
+    queryFn: async () => await fetchPokemon(pokemonName),
+  });
+
+  if (isPending) {
+    return <Loader></Loader>;
+  }
+
+  if (isError) {
+    // TODO: Error Pop up
+    return <span>Error: {error.message}</span>;
+  }
+
+  if (isSuccess) {
+    const url = data["sprites"]["other"]["official-artwork"]["front_default"];
+    return <PokeCard pokemonName={pokemonName} imgUrl={url}></PokeCard>;
+  }
 }
